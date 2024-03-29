831bb203ac3486ece1035e0fdb3ef37601b6631b
Khanh Chung <khanh.d.chung@gmail.com>
2024-03-28 21:29:52 -0700

    Initial Commit

---
+++ Inventix/InventixApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventixApp.swift
+//  Inventix
+//
+//  Created by Khanh Chung on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct InventixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
